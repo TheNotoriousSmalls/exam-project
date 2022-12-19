c868dc1322947851f27c4d0d457923614e4eebe9
Joakim Drougge <joakim.natvig@gmail.com>
2022-12-19 12:57:17 +0100

    Navigation + Added CSS

--- script/script.js
+++ script/script.js
@@ -41,3 +41,13 @@ function handleData(data) {
 
     });
 };
+
+// Menu 
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
