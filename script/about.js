d8c3fd9e223f877df530ee11d389f7b732a8cf6a
Joakim Drougge <joakim.natvig@gmail.com>
2022-12-20 15:43:45 +0100

    Structuring

---
+++ script/about.js
@@ -0,0 +1,46 @@
+const url = "https://keammd21-15ab.restdb.io/rest/about";
+const options = {
+    headers: {
+        'x-apikey': "63297716bf647d0a5c19859e"
+    },
+};
+
+fetch(url, options)
+    .then((response) => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        handleData(data);
+
+    })
+    .catch((e) => {
+        //Log error
+        console.error("An error occured:", e.message);
+    });
+
+function handleData(data) {
+    data.forEach((element) => {
+
+        // console.log(element);
+
+        const template = document.querySelector("template").content;
+        const templateClone = template.cloneNode(true);
+
+
+
+        templateClone.querySelector("#section-heading").innerHTML = element.heading;
+        templateClone.querySelector("#section-paragraph").innerHTML = element.paragraph;
+        // templateClone.querySelector("#about-image").src = element.image;
+
+
+        const mainEl = document.querySelector("main");
+
+        mainEl.appendChild(templateClone);
+
+    });
+
+
+};
